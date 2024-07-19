a4644c64d29315b10fd239d91cf116b3dc9fa267
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-07-19 18:26:02 +0200

    Update versionchecker.lua

--- server/versionchecker.lua
+++ server/versionchecker.lua
@@ -10,7 +10,7 @@ local function versionCheckPrint(_type, log)
 end
 
 local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/szileni/tbrp-companions/main/version.txt', function(err, text, headers)
+    PerformHttpRequest('https://raw.githubusercontent.com/Sadicius/hdrp-companions/main/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
         if not text then 
